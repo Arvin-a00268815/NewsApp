0536f041fcdb3c67f69ced70c8655cbde184c346
a00268815-arvin <a00268815@student.ait.ie>
2020-03-16 00:31:37 +0000

    error handling

--- app/src/main/java/com/example/newsapplication/repository/network/ErrorResponseHandler.kt
+++ app/src/main/java/com/example/newsapplication/repository/network/ErrorResponseHandler.kt
@@ -3,24 +3,41 @@ package com.example.newsapplication.repository.network
 import com.example.newsapplication.repository.CallBackListener
 import org.json.JSONObject
 import retrofit2.HttpException
+import java.net.UnknownHostException
 
 object ErrorResponseHandler {
 
     fun handle(throwable: Throwable, callBackListener: CallBackListener) {
-        val response = (throwable as HttpException).response()!!
 
-        with(response) {
-
-            errorBody()?.apply {
-
-
-                val jsonObject = JSONObject(string())
-
+        when(throwable){
+            is UnknownHostException -> {
                 callBackListener.onError(
-                    jsonObject.getString("code"),
-                    jsonObject.getString("message")
+                    "401", "No internet connection"
+                )
+            }
+            is HttpException ->{
+                val response = throwable.response()!!
+
+                with(response) {
+
+                    errorBody()?.apply {
+
+
+                        val jsonObject = JSONObject(string())
+
+                        callBackListener.onError(
+                            jsonObject.getString("code"),
+                            jsonObject.getString("message")
+                        )
+                    }
+                }
+            }
+            else ->{
+                callBackListener.onError(
+                    "401", "Something went wrong"
                 )
             }
         }
+
     }
 }
