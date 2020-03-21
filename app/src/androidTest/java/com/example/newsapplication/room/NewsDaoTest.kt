cd01e429d4b5b2c598e094649dea73f158e8d9f1
a00268815-arvin <a00268815@student.ait.ie>
2020-03-21 01:16:46 +0000

    dao test

--- app/src/androidTest/java/com/example/newsapplication/room/NewsDaoTest.kt
+++ app/src/androidTest/java/com/example/newsapplication/room/NewsDaoTest.kt
@@ -4,8 +4,10 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import com.example.newsapplication.model.News
 import com.example.newsapplication.room.dao.NewsDao
 import io.mockk.verify
+import io.reactivex.schedulers.Schedulers
 import org.junit.*
 import org.junit.runner.RunWith
 
@@ -27,6 +29,7 @@ class NewsDaoTest {
             .build()
 
         newsDao = appDatabase.newsDao()
+
     }
 
     @After
@@ -35,12 +38,31 @@ class NewsDaoTest {
     }
 
     @Test
-    fun testgetAll(){
+    fun test_isEmpty(){
+        val single = newsDao.getAll().subscribeOn(Schedulers.trampoline())
 
-
-        val single = newsDao.getAll()
-
-        Assert.assertTrue( single.test().values()[0].isEmpty() )
-        
+        single.test().assertValue {
+            it.isEmpty()
+        }
     }
+
+    @Test
+    fun test_getAll(){
+
+
+        val news = News(1,"JK Rowling", "Harry Potter", "Fiction")
+        val news2 = News(2,"JK Rowling", "Harry Potter", "Fiction")
+
+        newsDao.addNews(news).test()
+        newsDao.addNews(news2).test()
+
+        val single = newsDao.getAll().subscribeOn(Schedulers.trampoline())
+
+        single.test().assertValue {
+            it.size == 2
+        }
+        //Assert.assertEquals(list, single.test().values()[0])
+    }
+
+
 }
